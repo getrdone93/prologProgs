00d5f664930b36e73cb56bd909dda18728332700
Trever Anderson <TreverAnderson@Solutionary.com>
2016-10-04 15:50:22 -0500

    solved a list of prime numbers. aborted refactor

--- arithmetic.pl
+++ arithmetic.pl
@@ -19,19 +19,6 @@ has_factor(N,L) :-
 
 %A list of prime numbers
 %2.04
-matchList(L, L).
-primeNumberList(LB, UB, L) :-
-     generateList(LB, UB, L, []).
-generateList(UB, UB, Res, Res).
-generateList(LB, UB, L, Res) :-
-     LB =< UB,
-     is_prime(LB),
-     LBNew is LB + 1,
-     generateList(LBNew, UB, L, [LB | Res]).
-generateList(LB, UB, L, Res) :-
-     LBNew is LB + 1,
-     generateList(LBNew, UB, L, Res).
-
 primeNumberList(LB, UB, L) :-
      generateList(LB, UB, L, []).
 generateList(UB, UB, Res, Res).
