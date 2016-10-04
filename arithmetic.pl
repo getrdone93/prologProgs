80326413fc5bc78077ca4fbd867357fa773df3b1
Trever Anderson <TreverAnderson@Solutionary.com>
2016-10-04 15:14:30 -0500

    took solution from solution set as that one is better

--- arithmetic.pl
+++ arithmetic.pl
@@ -1,11 +1,18 @@
 %Determine whether a given integer number is prime
 %2.01
-is_prime(2) :- !.
-is_prime(N) :-
-     UB is div(N, 2),
-     checkPrime(N, UB, 3).
-checkPrime(_, N, N) :- !.
-checkPrime(N, UB, Inc) :-
-     mod(N, Inc) =\= 0,
-     IncNew is Inc + 1,
-     checkPrime(N, UB, IncNew).
+is_prime(2).
+is_prime(3).
+is_prime(P) :-
+     integer(P),
+     P > 3,
+     P mod 2 =\= 0,
+     \+ has_factor(P,3).
+
+% has_factor(N,L) :- N has an odd factor F >= L.
+%    (integer, integer) (+,+)
+has_factor(N,L) :-
+     N mod L =:= 0.
+has_factor(N,L) :-
+     L * L < N,
+     L2 is L + 2,
+     has_factor(N,L2).
