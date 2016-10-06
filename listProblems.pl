159bb85b5433b78c851801c8137e334d0b4990a5
Trever Anderson <TreverAnderson@Solutionary.com>
2016-10-06 13:21:34 -0500

    another idea

--- listProblems.pl
+++ listProblems.pl
@@ -120,11 +120,18 @@ dup([H | T], L1, [H, H | Res]) :-
 % dupN([], L, L, 0).
 % %dupN([H | T], L1, Res, N) :-
 
-genList(E, [E], 1) :- !.
-genList(E, [E | L], N) :-
-    genList(E, L, N1),
-    display(L),nl,
-    N is N1 - 1.
+% genList(E, [E], 1) :- !.
+% genList(E, [E | L], N) :-
+%     genList(E, L, N1),
+%     display(L),nl,
+%     N is N1 - 1.
+
+gList(E, L, N) :-
+     genList(E, L, N).
+genList(L, L, 0).
+genList(E, L, N) :-
+     genList(E, [E | L], N1),
+     N is N1 - 1.
 
 %Lib
 occurance(_, [], 0).
