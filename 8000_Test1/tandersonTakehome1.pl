988231859f4355079be16ea63231151417742f01
tanderson <treveranderson@unomaha.edu>
2016-10-16 17:43:04 -0500

    determined valid states

--- 8000_Test1/tandersonTakehome1.pl
+++ 8000_Test1/tandersonTakehome1.pl
@@ -26,3 +26,30 @@ reverseList(L1, L2) :-
 revList([], L2, L2) :- !.
 revList([H | T], L2, L3) :-
     revList(T, L2, [H | L3]).
+
+%3
+
+
+
+validState(M, C) :-
+    validMAndC(M, C),
+    M =:= 0,
+    !.
+validState(M, C) :-
+    validMAndC(M, C),
+    M =:= 3,
+    !.
+validState(M, C) :-
+    validMAndC(M, C),
+    MRight is 3 - M,
+    CRight is 3 - C,
+    MRight >= CRight,
+    M >= C.
+validMAndC(M, C) :-
+    M >= 0,
+    M =< 3,
+    C >= 0,
+    C =< 3.
+
+    % display(MOnRight),display(' '),display(COnRight),display(' '),
+    % display(RightVal),display(' '),display(LeftVal),display(' ')
