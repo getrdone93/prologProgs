abf2115bd5f4bfa055b658940c24038a3fe84138
Trever Anderson <TreverAnderson@Solutionary.com>
2016-11-06 17:48:36 -0600

    solved numba one son

---
+++ 8000_Assignments/a4/anderson.pl
@@ -0,0 +1,14 @@
+%1
+donald([D,O,N,A,L,D] +
+		 [G,E,R,A,L,D] =
+		[R,O,B,E,R,T]) :-
+	[D,O,N,A,L,G,E,R,B,T] ins 0..9,
+     all_different([D,O,N,A,L,G,E,R,B,T]),
+     100000*D + 10000*O + 1000*N + 100*A + 10*L + D +
+     100000*G + 10000*E + 1000*R + 100*A + 10*L + D
+     #= 100000*R + 10000*O + 1000*B + 100*E + 10*R + T,
+     label([D,O,N,A,L,G,E,R,B,T]).
+
+test2(A) :-
+     A ins 0..3,
+     display(A).
