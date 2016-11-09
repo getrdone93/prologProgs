02fb446385e80d8fb6426c937198e27838b31cf4
tanderson <treveranderson@unomaha.edu>
2016-11-08 22:11:03 -0600

    moved getLists inside of problem 2 space

--- anderson.pl
+++ anderson.pl
@@ -9,10 +9,6 @@ donald([D,O,N,A,L,D] +
      #= 100000*R + 10000*O + 1000*B + 100*E + 10*R + T,
      label([D,O,N,A,L,G,E,R,B,T]).
 
-getLists([(r1,r2), (r1,r4), (r1,r5), (r1,r6), (r2,r3), (r2,r4), (r2,r9),
-(r3,r4), (r3,r5), (r3,r9), (r4,r5), (r5,r6), (r5,r7), (r5,r9),
-(r6,r7), (r6,r8), (r7,r8), (r7,r9), (r8,r9)], [r1,r2,r3,r4,r5,r6,r7,r8,r9]).
-
 %2.
 threeColor(L) :-
 	getLists(RelationList, RegionList),
@@ -34,6 +30,9 @@ getColorList([H | T], Res, T1) :-
 	V in 1..3,
 	label([V]),
 	getColorList(T, Res, [color(H, V) | T1]).
+getLists([(r1,r2), (r1,r4), (r1,r5), (r1,r6), (r2,r3), (r2,r4), (r2,r9),
+	(r3,r4), (r3,r5), (r3,r9), (r4,r5), (r5,r6), (r5,r7), (r5,r9),
+	(r6,r7), (r6,r8), (r7,r8), (r7,r9), (r8,r9)], [r1,r2,r3,r4,r5,r6,r7,r8,r9]).
 
 %3
 assign(Profit, [W1, W2, W3, W4]):-
