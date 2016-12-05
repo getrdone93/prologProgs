0a5d53a04e60644118eee31ad938377c2882ce3f
tanderson <treveranderson@unomaha.edu>
2016-12-04 21:17:10 -0600

    got something halfway working

--- final.pl
+++ final.pl
@@ -1,17 +1,47 @@
 :- [clputility].
 
 schoolBus(OrderList) :-
-     StudentList = [student(DianaPlace, 'Diana', DianaStreet), student(BrucePlace, 'Bruce', BruceStreet),
-     student(TracyPlace, 'Tracy', TracyStreet), student(DannyPlace, 'Danny', DannyStreet),
-     student(JustinPlace, 'Justin', JustStreet)],
-     StreetTerms = [street(MulberryPlace, 'Mulberry'), street(BlueberryPlace, 'Blueberry'),
-     street(HuckleberryPlace, 'Huckleberry'), street(CherryPlace, 'Cherry'),
-     street(ElderberryPlace, 'Elderberry')],
-     StreetPlaces = [MulberryPlace, BlueberryPlace, HuckleberryPlace, CherryPlace, ElderberryPlace],
-     all_different()
-
-
-
-     DannyPlace #=< JustinPlace,
-     DannyPlace #=< TracyPlace,
-     JustinPlace #=< DianaPlace,
+    OrderList = [student(DianaPlace, 'Diana', DianaStreet), student(BrucePlace, 'Bruce', BruceStreet),
+    student(TracyPlace, 'Tracy', TracyStreet), student(DannyPlace, 'Danny', DannyStreet),
+    student(JustinPlace, 'Justin', JustinStreet)],
+    StreetOrder = [MulberryPlace, BlueberryPlace, HuckleberryPlace, CherryPlace, ElderberryPlace],
+    StreetList = [DianaStreet, BruceStreet, TracyStreet, DannyStreet, JustinStreet],
+    StudentOrder = [DianaPlace, BrucePlace, TracyPlace, DannyPlace, JustinPlace],
+    StudentOrder ins 1..5,
+    StreetList ins 1..5,
+    StreetOrder ins 1..5,
+    all_different(StudentOrder),
+    all_different(StreetOrder),
+    all_different(StreetList),
+    DannyPlace #< JustinPlace,
+    DannyPlace #< TracyPlace,
+    JustinPlace #< DianaPlace,
+    HuckleberryPlace #> CherryPlace,
+    HuckleberryPlace #< ElderberryPlace,
+    TracyPlace #> BrucePlace,
+    DianaStreet #\= 1,
+    DianaStreet #\= 3,
+    HuckleberryPlace #< BlueberryPlace,
+    MulberryPlace #< BlueberryPlace,
+    label(StudentOrder),
+    label(StreetOrder),
+    label(StreetList),
+    %name of second person off bus doesnt begin with D
+    checkNameOfSecondPerson(OrderList),
+    checkSecondToLastPerson(OrderList).
+checkSecondToLastPerson([student(4, _, Street) | _]) :-
+    Street #\= 2,
+    Street #\= 5.
+checkSecondToLastPerson([_ | T]) :-
+    checkSecondToLastPerson(T).
+checkNameOfSecondPerson([student(2, Name, _) | _]) :-
+    string_chars(Name, [FirstLetter | _]),
+    FirstLetter \== 'D',
+    !.
+checkNameOfSecondPerson([_ | T]) :-
+    checkNameOfSecondPerson(T).
+mapNumToStreet(1, 'Mulberry').
+mapNumToStreet(2, 'Blueberry').
+mapNumToStreet(3, 'Huckleberry').
+mapNumToStreet(4, 'Cherry').
+mapNumToStreet(5, 'Elderberry').
--- scrap.pl
+++ scrap.pl
@@ -1,3 +1,5 @@
-StreetList = [DianaStreet, BruceStreet, TracyStreet, DannyStreet, JustStreet],
+StreetTerms = [street(MulberryPlace, 'Mulberry'), street(BlueberryPlace, 'Blueberry'),
+street(HuckleberryPlace, 'Huckleberry'), street(CherryPlace, 'Cherry'),
+street(ElderberryPlace, 'Elderberry')],
 
 all_different(StreetList),
