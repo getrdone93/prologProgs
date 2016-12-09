451415fdd77776894242b087f2c3bbc92390e50c
tanderson <treveranderson@unomaha.edu>
2016-12-09 00:11:49 -0600

    trying to tie it all together

--- final/problem1.pl
+++ final/problem1.pl
@@ -6,25 +6,31 @@ edge(a, x).
 
 vertexCover(K, Vertices) :-
     vertexCover(K, 1, Vertices).
+% vertexCover(N, N, Vertices, Vertices).
 vertexCover(K, N, Vertices) :-
     N =< K,
     getAllNodes(AllNodes),
-    getNodeList(AllNodes, NodeList, N),
+    getNodeList(AllNodes, Vertices, N),
     getGraph(EdgeList),
-    cover(NodeList, EdgeList, Vertices).
+    cover(Vertices, EdgeList),
+    % vertexCover(N, N, Vertices, Res).
 vertexCover(K, N, Vertices) :-
     N1 is N + 1,
     vertexCover(K, N1, Vertices).
-cover(NodeList, EdgeList, Vertices) :-
-    getCoveredEdges(NodeList, EdgeList, Vertices),
-    subtract(EdgeList, Vertices, []).
+
+cover(NodeList, EdgeList) :-
+    getCoveredEdges(NodeList, EdgeList, TempCoveredEdges),
+    sort(TempCoveredEdges, CoveredEdges),
+    subtract(EdgeList, CoveredEdges, []).
+
 getCoveredEdges(NodeList, EdgeList, CoveredEdges) :-
-    getCoveredEdges(NodeList, EdgeList, CoveredEdges, []).
+    getCoveredEdges(NodeList, EdgeList, [], CoveredEdges).
 getCoveredEdges([], _, CoveredEdges, CoveredEdges).
 getCoveredEdges([Node | NodeList], EdgeList, CoveredEdges, Res) :-
     getEdges(Node, EdgeList, CoveredEdgesByNode),
     append(CoveredEdges, CoveredEdgesByNode, TempCoveredEdges),
     getCoveredEdges(NodeList, EdgeList, TempCoveredEdges, Res).
+
 getEdges(Node, EdgeList, CoveredEdgesByNode) :-
     getEdges(Node, EdgeList, CoveredEdgesByNode, []).
 getEdges(_, [], Res, Res).
@@ -36,6 +42,7 @@ getEdges(Node, [edge(X, Node) | T], [edge(X, Node) | Res], Result) :-
     !.
 getEdges(Node, [_ | T], Res, Result) :-
     getEdges(Node, T, Res, Result).
+
 getNodeList(AllNodes, NodeList, N) :-
     getNodeList(AllNodes, NodeList, [], N, 0).
 getNodeList(_, NodeList, NodeList, N, N).
@@ -45,6 +52,7 @@ getNodeList([Node | AllNodes], [Node | NodeList], Res, N, C) :-
 
 getGraph(Graph) :-
     findall(edge(X, Y), edge(X, Y), Graph).
+
 getAllNodes(AllNodes) :-
     findall(X, edge(X, Y), Xs),
     findall(Y, edge(X, Y), Ys),
