52a218a46ad9a3c02ecbc85ea00956484bb3d9c4
tanderson <treveranderson@unomaha.edu>
2016-12-10 15:32:41 -0600

    so so close

--- problem4.pl
+++ problem4.pl
@@ -1,20 +1,27 @@
 :- [clputility].
 
 schedule([Sun, Mon, Tue, Wed, Thu, Fri, Sat], TotalPayroll) :-
-    Sun #= 22,
-    Mon #= 17,
-    Tue #= 13,
-    Wed #= 14,
-    Thu #= 15,
-    Fri #= 18,
-    Sat #= 24,
-    [Bsun, Csun, Dsun, Esun, Fsun] ins 0..Sun, sum([Bsun, Csun, Dsun, Esun, Fsun], #>=, Sun),
-    [Cmon, Dmon, Emon, Fmon, Gmon] ins 0..Mon, sum([Cmon, Dmon, Emon, Fmon, Gmon], #>=, Mon),
-    [Atue, Dtue, Etue, Ftue, Gtue] ins 0..Tue, sum([Atue, Dtue, Etue, Ftue, Gtue], #>=, Tue),
-    [Awed, Bwed, Ewed, Fwed, Gwed] ins 0..Wed, sum([Awed, Bwed, Ewed, Fwed, Gwed], #>=, Wed),
-    [Athu, Bthu, Cthu, Fthu, Gthu] ins 0..Thu, sum([Athu, Bthu, Cthu, Fthu, Gthu], #>=, Thu),
-    [Afri, Bfri, Cfri, Dfri, Gfri] ins 0..Fri, sum([Afri, Bfri, Cfri, Dfri, Gfri], #>=, Fri),
-    [Asat, Bsat, Csat, Dsat, Esat] ins 0..Sat, sum([Asat, Bsat, Csat, Dsat, Esat], #>=, Sat),
+    [Bsun, Csun, Dsun, Esun, Fsun] ins 4..5,
+    [Cmon, Dmon, Emon, Fmon, Gmon] ins 3..4,
+    [Atue, Dtue, Etue, Ftue, Gtue] ins 2..3,
+    [Awed, Bwed, Ewed, Fwed, Gwed] ins 2..3,
+    [Athu, Bthu, Cthu, Fthu, Gthu] ins 3,
+    [Afri, Bfri, Cfri, Dfri, Gfri] ins 3..4,
+    [Asat, Bsat, Csat, Dsat, Esat] ins 4..5,
+    Sun #= Bsun + Csun + Dsun + Esun + Fsun,
+    Mon #= Cmon + Dmon + Emon + Fmon + Gmon,
+    Tue #= Atue + Dtue + Etue + Ftue + Gtue,
+    Wed #= Awed + Bwed + Ewed + Fwed + Gwed,
+    Thu #= Athu + Bthu + Cthu + Fthu + Gthu,
+    Fri #= Afri + Bfri + Cfri + Dfri + Gfri,
+    Sat #= Asat + Bsat + Csat + Dsat + Esat,
+    Sun #>= 22,
+    Mon #>= 17,
+    Tue #>= 13,
+    Wed #>= 14,
+    Thu #>= 15,
+    Fri #>= 18,
+    Sat #>= 24,
     label([Bsun, Csun, Dsun, Esun, Fsun]),
     label([Cmon, Dmon, Emon, Fmon, Gmon]),
     label([Atue, Dtue, Etue, Ftue, Gtue]),
@@ -22,10 +29,10 @@ schedule([Sun, Mon, Tue, Wed, Thu, Fri, Sat], TotalPayroll) :-
     label([Athu, Bthu, Cthu, Fthu, Gthu]),
     label([Afri, Bfri, Cfri, Dfri, Gfri]),
     label([Asat, Bsat, Csat, Dsat, Esat]),
-    TotalPayroll #= (Bsun + Csun + Dsun + Esun + Fsun) * 40
-    + (Cmon + Dmon + Emon + Fmon + Gmon) * 40
-    + (Atue + Dtue + Etue + Ftue + Gtue) * 40
-    + (Awed + Bwed + Ewed + Fwed + Gwed) * 40
-    + (Athu + Bthu + Cthu + Fthu + Gthu) * 40
-    + (Afri + Bfri + Cfri + Dfri + Gfri) * 40
-    + (Asat + Bsat + Csat + Dsat + Esat) * 40.
+    TotalPayroll #= Sun * 40
+    + Mon * 40
+    + Tue * 40
+    + Wed * 40
+    + Thu * 40
+    + Fri * 40
+    + Sat * 40.
