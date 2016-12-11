789072ee495e4264397855ee2c012d67029b1a50
tanderson <treveranderson@unomaha.edu>
2016-12-11 17:30:33 -0600

    got node out flow

---
+++ final/problem3.pl
@@ -0,0 +1,21 @@
+edge(s, a, 4).
+edge(s, b, 2).
+edge(a, c, 2).
+edge(a, b, 1).
+edge(a, d, 4).
+edge(c, t, 3).
+edge(b, d, 2).
+edge(d, t, 3).
+
+%edge(Outgoing, Incoming)
+
+getNodeOutFlow(Node, OutFlow) :-
+    findall(edge(OutNode, InNode, EdgeWeight), edge(OutNode, InNode, EdgeWeight), NodeList),
+    getOutFlow(Node, NodeList, 0, OutFlow).
+getOutFlow(_, [], OutFlow, OutFlow) :- !.
+getOutFlow(Node, [edge(Node, _, Weight) | NodeList], OutFlow, Res) :-
+    !,
+    TempOutFlow is OutFlow + Weight,
+    getOutFlow(Node, NodeList, TempOutFlow, Res).
+getOutFlow(Node, [_ | NodeList], OutFlow, Res) :-
+    getOutFlow(Node, NodeList, OutFlow, Res).
