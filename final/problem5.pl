78f3f4a033a377e52eb915ed59346389311a8b8c
tanderson <treveranderson@unomaha.edu>
2016-12-11 13:18:00 -0600

    can loop through and apply all unary ops in backtrack fashion

--- final/problem5.pl
+++ final/problem5.pl
@@ -14,12 +14,33 @@ binary(b(-)).
 binary(b(*)).
 binary(b(/)).
 
-equationBuilder(Equation, N) :-
-    N =:= 1,
-    fourPicker(FourList, N),
-    buildEquation(FourList, Equation).
-buildEquation(FourList, Equation) :-
-    //
+%must permute four list when n >= 2     permutation(FourList, PermFourList),
+
+equationBuilder(Answer, 1) :-
+    fourPicker(FourList, 1),
+    buildEquationOneFour(FourList, Answer).
+buildEquationOneFour([Four], Answer) :-
+    findall(Operation, unary(u(Operation)), UnaryOps),
+    execute(Four, UnaryOps, AnswerList).
+
+execute(Four, UnaryOps, AnswerList) :-
+    execute(Four, UnaryOps, [], AnswerList).
+execute(_, _, AnswerList, AnswerList).
+execute(Four, [+ | UnaryOps], AnswerList, Res) :-
+    Answer is 0 + Four,
+    execute(Four, UnaryOps, [Answer | AnswerList], Res).
+execute(Four, [- | UnaryOps], AnswerList, Res) :-
+    Answer is 0 - Four,
+    execute(Four, UnaryOps, [Answer | AnswerList], Res).
+execute(Four, [sqrt | UnaryOps], AnswerList, Res) :-
+    Answer is sqrt(Four),
+    execute(Four, UnaryOps, [Answer | AnswerList], Res).
+execute(Four, [! | UnaryOps], AnswerList, Res) :-
+    factorial(Four, Answer),
+    execute(Four, UnaryOps, [Answer | AnswerList], Res).
+execute(Four, [square | UnaryOps], AnswerList, Res) :-
+    Answer is Four ** 2,
+    execute(Four, UnaryOps, [Answer | AnswerList], Res).
 
 fourPicker(FourList, N) :-
     findall(pick(Four, Count), pick(Four, Count), AllFours),
@@ -28,6 +49,14 @@ fourPicker(FourList, AllFours, N) :-
     powerset(AllFours, FourList),
     length(FourList, Len),
     Len =:= N.
+
+
+factorial(0,1) :- !.
+factorial(N,F) :-
+   N > 0,
+   N1 is N - 1,
+   factorial(N1, F1),
+   F is N * F1.
 
 powerset([], []).
 powerset([_ | T], P) :-
