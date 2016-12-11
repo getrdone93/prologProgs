4450499956287f09d5e5b2bbcf03794f724484e7
tanderson <treveranderson@unomaha.edu>
2016-12-10 19:25:19 -0600

    givens are typed in

---
+++ final/problem5.pl
@@ -0,0 +1,15 @@
+pick(4, 1).
+pick(44, 2).
+pick(0.4, 1).
+pick(infinity(0.4), 1).
+
+unary(u(+)).
+unary(u(-)).
+unary(u(sqrt)).
+unary(u(!)).
+unary(u(square)).
+
+binary(b(+)).
+binary(b(-)).
+binary(b(*)).
+binary(b(/)).
