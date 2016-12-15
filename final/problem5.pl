29a7453b2a7ed81b72cbc85890dd17680eeeefc2
tanderson <treveranderson@unomaha.edu>
2016-12-14 21:04:28 -0600

    have an idea for generating fours

--- final/problem5.pl
+++ final/problem5.pl
@@ -15,9 +15,50 @@ binary(b(*)).
 binary(b(/)).
 
 
+getFourList(FourList) :-
+    getFourList(FourList, 4).
+getFourList(FourList, N) :-
+    findall(pick(F, C), pick(F, C), Initial),
+    getFourList(Initial, FourList, N),
+    N1 is N - 1,
+    getFourList(FourList, N1).
+getFourList([H | _], FourList, 4) :-
+    C =:= 4,
+    duplicateNTimes([H], C, FourList).
+getFourList([H | Init], FourList, 3) :-
+    duplicateNTimes([H], 3, Temp),
+    member(L, Init),
+    append(Temp, [L], FourList).
+getFourList([H | Init], FourList, 2) :-
+    duplicateNTimes([H], 2, Temp),
+    getPowerSet(Init, 2, EndList),
+    append(Temp, EndList, FourList).
+
 validFourList(FourList) :-
     validFourList(FourList, 0).
 validFourList([], 4).
 validFourList([pick(_, C) | List], Count) :-
     TempCount is Count + C,
     validFourList(List, TempCount).
+
+duplicateNTimes(L1, N, L2) :-
+    duplicateNTimes(L1, N, L2, N).
+duplicateNTimes([],_,[],_) :- !.
+duplicateNTimes([_ | Xs], N, Ys, 0) :-
+    duplicateNTimes(Xs, N, Ys, N),
+    !.
+duplicateNTimes([X | Xs], N, [X | Ys], K) :-
+     K > 0,
+     K1 is K - 1,
+     duplicateNTimes([X | Xs], N, Ys, K1).
+
+ getPowerSet(List, N, Res) :-
+     powerset(List, Res),
+     length(Res, Len),
+     Len =:= N.
+
+ powerset([], []).
+ powerset([_ | T], P) :-
+     powerset(T, P).
+ powerset([H | T], [H | P]) :-
+     powerset(T, P).
